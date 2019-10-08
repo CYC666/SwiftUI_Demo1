9dfd48fdd7970d8ef8f18caca027837deb85c2dd
sm <>
2019-10-08 11:00:33 +0800

    s

---
+++ SwiftUI_Demo1/SwiftUI_Demo1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Demo1
+//
+//  Created by sm on 2019/10/8.
+//  Copyright © 2019 CYC666. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
